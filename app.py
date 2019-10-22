df99949c8a56d46a926cc61f01d5c24ba5637b6c
Arpit Pokhriyal <nickpokhriyal@gmail.com>
2019-10-22 10:09:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 from flask import Flask,render_template,request
 import pickle
-from pyt import Detect
-
-UPLOAD_FOLDER = '/home/me/Desktop/arpit/MachineLearning/License plate detection/uploads'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+from livereload import Server
 
 app=Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-@app.route('/')
+@app.route('/'
 def home():
     return render_template('home.html')
 
@@ -20,15 +12,34 @@ def home():
 def result():
     if request.method=='POST':
         file=request.files.get('myfile')
-        plate_num=Detect(file)
-        print(plate_num)
-        #if file and allowed_file(file.filename):
-            #filename = secure_filename(file.filename)
-            #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            #return redirect(url_for('uploaded_file', filename=filename))
-    #filename = 'http://127.0.0.1:5000/uploads/' + filename
+        filename=file
+        #from skimage.io import imread
+        import matplotlib.pyplot as plt
+        car_image = plt.imread(filename)
+        from PIL import Image
+        #car_image = Image.open(filename)
+        car_image=Image.fromarray(car_image)
+        car_image.save("static/car.png")
+        car_image.save("car.png")
+        import requests
+        regions = ['fr', 'it']
+        with open('car.png', 'rb') as fp:
+            response = requests.post(
+                'https://api.platerecognizer.com/v1/plate-reader/',
+                data=dict(regions=regions),  # Optional
+                files=dict(upload=fp),
+                headers={'Authorization': 'Token 2ec5878b06ca1be24f61f34142443cc5baa3194c'})
+        #print(response.json())
+        #print(response.json()['results'][0]['plate'])
+        #plate_num=Detect(file)
+        plate_num=response.json()['results'][0]['plate']
+        plate_num=plate_num.upper()
+        #print(plate_num)
         return render_template('result.html', plate = plate_num)
 
 if __name__=='__main__':
+    server = Server(app.wsgi_app)
+    server.serve()
+    server.watch('/Views/*')
     #app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(debug=True) 
+    app.run(debug=True)
